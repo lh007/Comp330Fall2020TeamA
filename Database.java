43654f9c44e7e9692da1e63f29b719e4236f7f36
taramla <tla2@luc.edu>
2020-10-01 19:17:19 -0500

    improved read in, new methods, getters and setters

--- Database.java
+++ Database.java
@@ -1,48 +1,96 @@
+/*
+  Database Class
+
+  @author tarala
+ * reads in file
+
+ */
+
+
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Database {
     String familyFile = "FamilyTreeInputTextFile.csv";
-    ArrayList<Person> individuals = new ArrayList<>();
-    ArrayList<Person> relationships = new ArrayList<>();
-    private void readFile() throws Exception { // reads in the file
-        String fileName = this.familyFile;
-        String data = readFileAsString(fileName);
-        String[] informationArray = data.split("\n"); // array of strings called riddleArray is split by /
-        for (String i: informationArray){
-            Person currentPerson = this.makePerson(i);
-            if (currentPerson.getPersonNum().contains("P")){
-                individuals.add(currentPerson);
-            }
-            if (currentPerson.getPersonNum().contains("R")){
-                relationships.add(currentPerson);
-            }
-        }
+    String[] personArray;
+    String[] relationshipsArray;
+    HashMap<String, Person> db = new HashMap<>();
 
+    public Database() throws Exception {
+        this.readFile();
     }
-    private Person makePerson(String data) {
-        String[] personArray = data.split(",");
-        String personNum = personArray[0];
-        String lastName = personArray[1];
-        String firstName =personArray[2];
-        String nameSuffix = personArray[3];
-        String birthday = personArray[4];
-        String birthCity = personArray[5];
-        String deathDate = personArray[6];
-        String deathCity = personArray[7];
-        String relationship = personArray[8];
-        Person result = new Person(personNum, lastName, firstName, nameSuffix, birthday, birthCity, deathDate, deathCity, relationship);
-        return result;
-    }
-    public String readFileAsString(String fileName)throws Exception {
+
+    public String readFileAsString(String fileName) throws Exception {
         String data = "";
         data = new String(Files.readAllBytes(Paths.get(fileName)));
         return data;
 
     }
-    public void main(String[] args){
-        System.out.println(individuals);
+    private void readFile() throws Exception { // reads in the file
+        String fileName = this.familyFile;
+        String data = readFileAsString(fileName);
+        String[] informationArray = data.split("\n"); // array of strings called riddleArray is split by /
+        for (int i = 0; i <= informationArray.length; i++) {
+            String currentLine = informationArray[i];
+            if (currentLine.startsWith("P")) {
+                Person currentPerson = this.makePerson(currentLine);
+                db.put(currentPerson.getPersonNum(), currentPerson);
+            }
+//creates formatting of relationship array
+            if (currentLine.startsWith("R")) {
+                relationshipsArray = currentLine.split(",");
+                String relationshipNum = relationshipsArray[0];
+                String wife = relationshipsArray[1];
+                String husband = relationshipsArray[2];
+                String marriageDate = relationshipsArray[3];
+                String divorceDate = relationshipsArray[4];
+                String marriageLocation = relationshipsArray[5];
+                createMarriage(relationshipNum, wife, husband, marriageDate, divorceDate, marriageLocation);
+
+            }
+        }
+
     }
-}
+
+    private Person makePerson(String data) {
+        personArray = data.split(",");
+        String personNum = personArray[0];
+        String lastName = personArray[1];
+        String firstName = personArray[2];
+        String nameSuffix = personArray[3];
+        String birthday = personArray[4];
+        String birthCity = personArray[5];
+        String deathDate = personArray[6];
+        String deathCity = personArray[7];
+        String parentRelationship = personArray[8];
+        Person result = new Person(personNum, lastName, firstName, nameSuffix, birthday, birthCity, deathDate, deathCity, parentRelationship);
+        return result;
+    }
+
+//creates marriage relationship using index 1 and 2 of relationship array respectively
+    public void createMarriage(String relationshipNum, String wife, String husband, String marriageDate, String divorceDate, String marriageLocation) {
+        Person female = db.get(wife);
+        Person male = db.get(husband);
+        female.setSpouses(male);
+        for (Person p: db.values()){
+            if (p.getParentRelationship().equals(relationshipNum)){
+                p.setParents(female, male);
+            }
+        }
+
+    }
+
+    public void createSiblings(){
+        for (Person p1: db.values()){
+            for (Person p2: db.values()){
+                if (p1.getParents().equals(p2.getParents())){
+                    p1.setSiblings(p2);
+                }
+                }
+            }
+        }
+    }
+
+
 
--- Person.java
+++ Person.java
@@ -1,3 +1,15 @@
+/*
+  Person class
+
+  @author tarala
+ * @version 2
+ * attributes, getters and setters, create relationships (parents,sibling,marriage)
+ */
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Person {
     private String personNum;
     private String lastName;
@@ -7,7 +19,12 @@ public class Person {
     private String birthCity;
     private String deathDate;
     private String deathCity;
-    private String relationship;
+    private String parentRelationship;
+    private String partner;
+    private ArrayList parents;
+    private ArrayList siblings;
+    private ArrayList children;
+
 
 
     public Person(String personNum, String lastName, String firstName, String nameSuffix, String birthday,
@@ -20,10 +37,114 @@ public class Person {
         this.birthCity = birthCity;
         this.deathDate = deathDate;
         this.deathCity = deathCity;
-        this.relationship = relationship;
+        this.parentRelationship = relationship;
+        parents = new ArrayList<Person>();
+        siblings = new ArrayList<Person>();
+        children = new ArrayList<Person>();
 
     }
+
+    public String relationship(){
+        return "";
+    }
+    public void setParents(Person p1, Person p2) {
+        parents.add(Arrays.asList(p1, p2));
+        //p1.children.add(child);
+        //p2.children.add(child);
+
+    }
+    public void setSpouses(Person partner2){
+        if (this.getSpouse().equals("") && partner2.equals("")){
+            this.partner = partner2.getFirstName();
+            partner2.partner = this.getFirstName();
+        }
+    }
+
+    public void setSiblings(Person sibling2){
+        this.siblings.add(sibling2);
+        sibling2.siblings.add(this);
+
+    }
+
     public String getPersonNum(){
         return personNum;
+    }
+
+    public void setPersonNum(String personNum) {
+        this.personNum = personNum;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getNameSuffix() {
+        return nameSuffix;
+    }
+
+    public void setNameSuffix(String nameSuffix) {
+        this.nameSuffix = nameSuffix;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getBirthCity() {
+        return birthCity;
+    }
+
+    public void setBirthCity(String birthCity) {
+        this.birthCity = birthCity;
+    }
+
+    public String getDeathDate() {
+        return deathDate;
+    }
+
+    public void setDeathDate(String deathDate) {
+        this.deathDate = deathDate;
+    }
+
+    public String getDeathCity() {
+        return deathCity;
+    }
+
+    public void setDeathCity(String deathCity) {
+        this.deathCity = deathCity;
+    }
+
+    public String getParentRelationship() {
+        return parentRelationship;
+    }
+    public String getSpouse(){
+        return partner;
+    }
+    public ArrayList<Person> getChildren() {
+        return children;
+    }
+
+    public ArrayList<Person> getParents() {
+        return parents;
+    }
+
+    public ArrayList<Person> getSiblings() {
+        return siblings;
     }
 }
