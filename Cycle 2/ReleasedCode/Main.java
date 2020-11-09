7a0aebaefd3cf67cc4ec60b1dff32356dd92f40b
taramla <tla2@luc.edu>
2020-11-08 19:21:06 -0600

    correcting folder

--- Cycle 2/ReleasedCode/Main.java
+++
@@ -1,9 +0,0 @@
-
-
-public class Main {
-    public static void main(String [] args) throws Exception {
-        Database d = new Database();
-        System.out.println();
-    }
-
-}
--- Cycle 2/ReleasedCode/README.md
+++
@@ -1,2 +0,0 @@
-# Comp330Fall2020TeamA
-For your team work; private to your team
