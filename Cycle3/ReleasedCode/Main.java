be9de5dc9363107744b6dcf4a68de06d496ba1f5
taramla <tla2@luc.edu>
2020-12-02 09:59:07 -0600

    cycle 3 initial and different way of searchMember

---
+++ Cycle3/ReleasedCode/Main.java
@@ -0,0 +1,9 @@
+package Cycle3.ReleasedCode;
+
+public class Main {
+    public static void main(String [] args) throws Exception {
+        Database d = new Database();
+        System.out.println();
+    }
+
+}
