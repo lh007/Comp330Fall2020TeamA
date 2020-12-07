3c310687b4495ee409de9f79fefde1a91ed3697c
taramla <tla2@luc.edu>
2020-12-07 15:03:07 -0600

    delete test case folder

--- Cycle3/ReleasedCode/Main.java
+++ Cycle3/ReleasedCode/Main.java
@@ -3,7 +3,7 @@ package Cycle3.ReleasedCode;
 public class Main {
     public static void main(String [] args) throws Exception {
         Database d = new Database();
-        System.out.println();
+
     }
 
 }
